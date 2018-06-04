d182f46fdbbe7e169fa8d3b02af4fea58b95939b
ZhaoGuan <ZhaoGuan@users.noreply.github.com>
2018-06-04 16:41:25 +0800

    fix

--- kafka_consuer.py
+++ kafka_consuer.py
@@ -5,14 +5,16 @@ import geoip2.database
 import Geohash.geohash as geohash
 from kafka import KafkaConsumer
 from influxdb import InfluxDBClient
+import os
 
+PATH = os.path.dirname(os.path.abspath(__file__))
 consumer = KafkaConsumer('emoji_appstore', bootstrap_servers='kika-data-gimbal0.intranet.com:9092',
                          group_id='Model_DashBoard')
 client = InfluxDBClient(host='0.0.0.0', port=8086, username='root', password='root', database='popup_geohash')
 
 
 def ip_to_genhash(ip):
-    with geoip2.database.Reader('./GeoIP2-City.mmdb') as reader:
+    with geoip2.database.Reader(PATH + '/GeoIP2-City.mmdb') as reader:
         print(ip)
         response = reader.city(ip)
         # country = response.country.iso_code
@@ -48,7 +50,10 @@ def one_consumer():
             try:
                 tag = log_json['extra']['tag']
             except:
-                tag = log_json['extra']['tags']
+                try:
+                    tag = log_json['extra']['tags']
+                except:
+                    tag = log_json['extra']['key_word']
             genhash_result = ip_to_genhash(ip)
             json_body = [{
                 "measurement": "country",
